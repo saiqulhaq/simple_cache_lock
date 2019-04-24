d2ee4a71f40eb414de123bdec7be4c3f987ad1c0
Saiqul Haq <saiqulhaq@gmail.com>
2019-04-24 09:24:45 +0700

    init project

---
+++ lib/simple_cache_lock.rb
@@ -0,0 +1,6 @@
+require "simple_cache_lock/version"
+
+module SimpleCacheLock
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/simple_cache_lock/version.rb
@@ -0,0 +1,3 @@
+module SimpleCacheLock
+  VERSION = "0.1.0"
+end
